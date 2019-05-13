d7b38fcfa80e43d11e1b7eb666ba8144670271ff
szubarcz <paulina.szubarczyk@nokia.com>
2019-05-13 23:32:52 +0200

    optimise lookup of tags per module

--- rplugin/python3/high_ttcn3.py
+++ rplugin/python3/high_ttcn3.py
@@ -23,34 +23,39 @@ class TestPlugin(object):
         }
 
     module_import_regex = re.compile('import\s+from\s+(\w*)')
+    module_from_file_regex = re.compile("\/(\w*)\.ttcn3")
 
     def __init__(self, nvim):
         self.nvim = nvim
         tags_file = open(os.path.abspath("./tags"))
-        self.tags = [[i.strip() for i in t.split("\t")]for t in tags_file.readlines()]
+        self.tags = {}
+        for t in tags_file.readlines():
+            tag = [i.strip() for i in t.split("\t")]
+            module = self.module_from_file_regex.search(tag[1]).group(1)
+            if module in self.tags:
+                self.tags[module].append(tag)
+            else:
+                self.tags[module] = [tag]
 
     @pynvim.function('HighlightTtcn')
     def highlight_ttcn(self, filename):
         file = open(filename[0], "r").readlines()
+        target_files = self._get_imported_modules(file)
 
-        target_files = self._get_imported_files(file)
-        target_files.append(filename[0])
-        self.nvim.out_write(str(target_files))
-        for tag in self.tags:
-            if tag[1] in target_files:
-                self.nvim.command("syn keyword ttcn3_"+self.mappings[tag[3].strip()] + " " + tag[0])
-        self.nvim.out_write("\n")
+        module = self.module_from_file_regex.search(filename[0]).group(1)
+        target_files.append(module)
 
-    def _get_imported_files(self, file_content):
+        for f in target_files:
+            if f in self.tags:
+                for tag in self.tags[f]:
+                    self.nvim.command("syn keyword ttcn3_"+self.mappings[tag[3].strip()] + " " + tag[0])
+
+    def _get_imported_modules(self, file_content):
         result = []
         for line in file_content:
              import_match = self.module_import_regex.search(line)
              if import_match :
-                 try:
-                     tag = next(t for t in self.tags if t[0] == import_match.group(1))
-                     result.append(tag[1])
-                 except:
-                     pass
+                 result.append(import_match.group(1))
         return result
 
 
